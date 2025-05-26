595e3f3acfe32dd888e2e4f9aad92ad17d8bf1b4
shikhatjp@gmail.com <shikhatjp@gmail.com>
2025-05-26 13:51:26 +0530

    first commit

---
+++ currrency_convertor.js
@@ -0,0 +1,36 @@
+import https from 'https';
+import readline from 'readline';
+import chalk from 'chalk';
+
+const rl = readline.createInterface({   //to interact with cli
+    input: process.stdin,               //read data
+    output: process.stdout              //write data in cli
+});
+
+const apikey = '6f1cf92123939c3f5359b738';
+const url = `https://v6.exchangerate-api.com/v6/${apikey}/latest/USD`;
+
+const convertCurrency = (amount , rate ) =>{
+    return (amount * rate).toFixed(2);
+}
+
+https.get(url ,(response) =>{
+    response.on('data' , (chunk) => {
+        data += chunk;
+    });
+    response.on('end' , () => {
+        const data = JSON.parse(data).conversion_rates;
+        rl.question("Enter the amount you want to convert: ", (amount) => {
+            rl.question("Enter the target currency you want to convert to(eg: INR, EUR, NPR): ", (currency) => {
+                const rate = rate[currency.toUpperCase()];
+                if (rate) {
+                    console.log(`${amount} USD is equal to ${convertCurrency(amount , rate )} ${currency}`);
+                } else {
+                    console.log("Invalid currency code. Please try again.");
+                }
+               rl.close();
+            })
+        })
+
+    })
+})
